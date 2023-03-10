58cf89ed70f227702c8d9e7c176b2433c4f71cd0
nanooka <102428722+nanooka@users.noreply.github.com>
2023-03-11 02:41:05 +0400

    submit with 'enter'

--- email.js
+++ email.js
@@ -1,5 +1,6 @@
 const email = document.getElementById("email-input");
-const error = document.getElementById("email-validation");
+const error = document.querySelector(".error");
+const correct = document.querySelector(".correct");
 const submitButton = document.getElementById("submit-button");
 
 let mailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
@@ -7,30 +8,24 @@ let mailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 function testMail(input) {
   if (mailRegex.test(input.value)) {
     console.log("this is true");
-    input.classList.add("correct");
-    input.classList.remove("error");
-    error.firstElementChild.style.display = "none";
-    error.lastElementChild.style.display = "block";
+    error.classList.remove("visible");
+    correct.classList.add("visible");
   } else if (!mailRegex.test(input.value)) {
     console.log("this is false");
-    input.classList.add("error");
-    input.classList.remove("correct");
-    error.firstElementChild.style.display = "block";
-    error.lastElementChild.style.display = "none";
+    correct.classList.remove("visible");
+    error.classList.add("visible");
   }
 }
+
 submitButton.addEventListener("click", (event) => {
   event.preventDefault();
   testMail(email);
   console.log("clicked");
 });
 
-// function ValidateEmail(mail) {
-//   if (
-//     /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(myForm.emailAddr.value)
-//   ) {
-//     return true;
-//   }
-//   alert("You have entered an invalid email address!");
-//   return false;
-// }
+email.addEventListener("keypress", function (event) {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    document.getElementById("submit-button").click();
+  }
+});
